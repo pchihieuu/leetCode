490ca8cb92729bc5b9588030fc6a26071d7d0c55
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 04:02:18 +0700

    Add files via upload

---
+++ leetcode83.go
@@ -0,0 +1,20 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	cur := head
+	for cur != nil && cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
