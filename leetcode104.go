48968b4686b156bc96c656ebd030772c628d97f2
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 04:29:21 +0700

    Add files via upload

---
+++ leetcode104.go
@@ -0,0 +1,23 @@
+package main
+
+//104. Maximum Depth of Binary Tree
+//author: Phan Chi Hieu
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	}
+	return rightDepth + 1
+}
