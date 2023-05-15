9ddf83e85693af1f3e60e28fc88daf722bf55ee5
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 04:56:27 +0700

    Add files via upload

---
+++ leetcode142.java
@@ -0,0 +1,30 @@
+public class leetcode142 {
+    //142. Linked List Cycle II
+    //author: Phan Chi Hieu
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+         ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        if (fast == null || fast.next == null) return null;
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+}
+}
