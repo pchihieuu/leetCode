06d8a1ac3e9708d633414791807acb35ad919a44
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 05:29:43 +0700

    Add files via upload

---
+++ leetcode206.go
@@ -0,0 +1,21 @@
+package main
+
+//206. Reverse Linked List
+//author: Phan Chi Hieu
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+	}
+	return prev
+}
