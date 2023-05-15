781c34fc3a3fdcf149f129ea9c4c140f319b1a74
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 05:35:51 +0700

    Add files via upload

---
+++ leetcode210.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+public class leetcode210 {
+   //
+   //author: Phan Chi Hieu
+   public int[] findOrder(int numCourses, int[][] prerequisites) {
+    List<Integer> ans = new ArrayList<>();
+    List<Integer>[] graph = new List[numCourses];
+    int[] inDegree = new int[numCourses];
+
+    for (int i = 0; i < numCourses; ++i)
+        graph[i] = new ArrayList<>();
+
+    // Build graph
+    for (int[] p : prerequisites) {
+    final int u = p[1];
+    final int v = p[0];
+    graph[u].add(v);
+    ++inDegree[v];
+    }
+
+// Topology
+    Queue<Integer> q = IntStream.range(0, numCourses)
+                       .filter(i -> inDegree[i] == 0)
+                       .boxed()
+                       .collect(Collectors.toCollection(ArrayDeque::new));
+
+    while (!q.isEmpty()) {
+    final int u = q.poll();
+    ans.add(u);
+    for (final int v : graph[u])
+        if (--inDegree[v] == 0)
+        q.offer(v);
+    }
+
+    if (ans.size() == numCourses)
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    return new int[] {};
+    } 
+}
