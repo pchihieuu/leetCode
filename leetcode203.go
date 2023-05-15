e3571fda078edf94098fd706a7250ae7dec6c615
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 05:25:13 +0700

    Add files via upload

---
+++ leetcode203.go
@@ -0,0 +1,25 @@
+package main
+
+//203. Remove Linked List Elements
+//author: Phan Chi Hieu
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	for head != nil && head.Val == val {
+		head = head.Next
+	}
+	cur := head
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
