52b6c29528dfae24fad37589054d57fdab19e60f
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 05:04:05 +0700

    Add files via upload

---
+++ leetcode167.go
@@ -0,0 +1,18 @@
+package main
+
+//167. Two Sum II - Input Array Is Sorted
+//author: Phan Chi Hieu
+func twoSum(numbers []int, target int) []int {
+	i := 0
+	j := len(numbers) - 1
+	for i < j {
+		if numbers[i]+numbers[j] == target {
+			return []int{i + 1, j + 1}
+		} else if numbers[i]+numbers[j] < target {
+			i++
+		} else {
+			j--
+		}
+	}
+	return []int{1, 1}
+}
