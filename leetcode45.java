3549c1956e252229f165cd2e7b132dac51d08e4e
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 03:43:50 +0700

    Add files via upload

---
+++ leetcode45.java
@@ -0,0 +1,24 @@
+public class leetcode45 {
+    //Jump Game II
+    // author: Phan Chi Hieu
+    public int jump(int[] nums) {
+        int ans = 0;
+        int end = 0;
+        int farthest = 0;
+
+    // Implicit BFS
+        for (int i = 0; i < nums.length - 1; ++i) {
+            farthest = Math.max(farthest, i + nums[i]);
+                if (farthest >= nums.length - 1) {
+                    ++ans;
+                break;
+        }
+        if (i == end) {   // Visited all the items on the current level
+            ++ans;          // Increment the level
+            end = farthest; // Make the queue size for the next level
+        }
+    }
+
+        return ans;
+  }
+}
