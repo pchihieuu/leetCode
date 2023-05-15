d6e1fabca5d3d6bd6af41155fc80ab5ad71f8d99
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 03:51:37 +0700

    Add files via upload

---
+++ leetcode57.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class leetcode57 {
+    //Insert Interval
+    //author: Phan Chi Hieu
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        final int n = intervals.length;
+        List<int[]> ans = new ArrayList<>();
+        int i = 0;
+
+        while (i < n && intervals[i][1] < newInterval[0])
+        ans.add(intervals[i++]);
+
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            ++i;
+        }
+
+        ans.add(newInterval);
+
+        while (i < n)
+            ans.add(intervals[i++]);
+
+        return ans.stream().toArray(int[][] ::new);
+    }
+}
