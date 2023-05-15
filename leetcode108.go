cabaceab24761e7f511469b3a131758140501ac2
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-16 04:32:36 +0700

    Add files via upload

---
+++ leetcode108.go
@@ -0,0 +1,23 @@
+package main
+
+//108. Convert Sorted Array to Binary Search Tree
+//author: Phan Chi Hieu
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+}
