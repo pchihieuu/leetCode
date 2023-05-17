44ff7bba38b4ceafaabd0bb5f2e75dfa0df9ddba
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-17 21:48:29 +0700

    Add files via upload

---
+++ leetcode237.go
@@ -0,0 +1,15 @@
+package main
+
+//237. Delete Node in a Linked List
+//author: Phan Chi Hieu
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
