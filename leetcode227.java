0e11fd49133de8dcec4a933b4072edda9700c771
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-17 21:33:49 +0700

    Add files via upload

---
+++ leetcode227.java
@@ -0,0 +1,30 @@
+public class leetcode227 {
+    //227. Basic Calculator II
+    //author: Phan Chi Hieu
+    public int calculate(String s) {
+        int ans = 0;
+        int currNum = 0;
+        int prevNum = 0;
+        char op = '+';
+
+        for (int i = 0; i < s.length(); ++i) {
+            final char c = s.charAt(i);
+            if (Character.isDigit(c))
+                currNum = currNum * 10 + (c - '0');
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1) {
+            if (op == '+' || op == '-') {
+                ans += prevNum;
+                prevNum = op == '+' ? currNum : -currNum;
+            } else if (op == '*') {
+                prevNum *= currNum;
+            } else if (op == '/') {
+                prevNum /= currNum;
+        }
+            op = c;
+            currNum = 0;
+      }
+    }
+
+    return ans + prevNum;
+    }
+}
