9b580079430fb4c9ba3ca2e0c463f6a1f3dc9419
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-25 15:16:31 +0700

    Add files via upload

---
+++ leetcode216.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class leetcode216 {
+    /* 216. Combination Sum III
+     * author: Phan Chi Hieu
+     */
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(k, n,1, new ArrayList<>(), ans);
+        return ans;
+        
+    }
+    private void dfs(int k, int n, int s, List<Integer> path, List<List<Integer>> ans) {
+        if(k == 0 && n == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        if(k == 0 || n < 0) {
+            return;
+        }
+        for(int i = s; i <=9; ++i) {
+            path.add(i);
+            dfs(k - 1, n - i, i + 1, path, ans);
+            path.remove(path.size() - 1);
+        }
+    }
+}
