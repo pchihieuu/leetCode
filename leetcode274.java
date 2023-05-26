972d95976416869efa167fbe6296801c709e429c
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-26 23:31:37 +0700

    Add files via upload

---
+++ leetcode274.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class leetcode274 {
+    public int hIndex(int[] citations) {
+        final int n = citations.length;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < n; ++i)
+            if (citations[i] >= n - i)
+                return n - i;
+
+        return 0;
+    }
+}
