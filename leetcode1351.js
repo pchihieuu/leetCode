3e48cd28fd12afde71a0b2e55b7b1ed32aec716e
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-06-09 04:10:13 +0700

    Add files via upload

---
+++ leetcode1351.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1351 lang=javascript
+ *
+ * [1351] Count Negative Numbers in a Sorted Matrix
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function (grid) {
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] < 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
+// @lc code=end
