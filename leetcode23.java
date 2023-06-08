04e79918a32d9b3fb86283e148c1e7a2824bba80
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-06-09 04:19:52 +0700

    Add files via upload

---
+++ leetcode23.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class leetcode23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        Queue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (final ListNode list : lists)
+            if (list != null)
+                minHeap.offer(list);
+
+            while (!minHeap.isEmpty()) {
+            ListNode minNode = minHeap.poll();
+            if (minNode.next != null)
+                minHeap.offer(minNode.next);
+                curr.next = minNode;
+                curr = curr.next;
+            }
+
+        return dummy.next;
+    }
+}
