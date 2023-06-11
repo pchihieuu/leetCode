020009e0b1c22f3adba52a7182425f8d99868b71
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-11 23:40:30 +0700

    Add files via upload

---
+++ leetcode77.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class leetcode77 {
+   public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(n, k, 1, new ArrayList<>(), ans);
+        return ans;
+    }
+     private void dfs(int n, int k, int s, List<Integer> path, List<List<Integer>> ans) {
+    if (path.size() == k) {
+      ans.add(new ArrayList<>(path));
+      return;
+    }
+
+    for (int i = s; i <= n; ++i) {
+      path.add(i);
+      dfs(n, k, i + 1, path, ans);
+      path.remove(path.size() - 1);
+    }
+ } 
+}
