50043e3baea037d19cba7a19e13975195bfd2901
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-11 23:19:04 +0700

    Add files via upload

---
+++ leetcode49.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class leetcode49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> keyToAnagrams = new HashMap<>();
+
+        for (final String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            keyToAnagrams.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(keyToAnagrams.values());
+    }
+}
