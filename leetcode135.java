363d45b6ba7c5af7ebec79754796f1c42b3cb7d8
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-13 16:37:05 +0700

    Add files via upload

---
+++ leetcode135.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class leetcode135 {
+    public int candy(int[] ratings) {
+        final int n = ratings.length;
+
+        int ans = 0;
+        int[] l = new int[n];
+        int[] r = new int[n];
+        Arrays.fill(l, 1);
+        Arrays.fill(r, 1);
+
+        for (int i = 1; i < n; ++i)
+            if (ratings[i] > ratings[i - 1])
+                l[i] = l[i - 1] + 1;
+
+        for (int i = n - 2; i >= 0; --i)
+            if (ratings[i] > ratings[i + 1])
+                r[i] = r[i + 1] + 1;
+
+        for (int i = 0; i < n; ++i)
+            ans += Math.max(l[i], r[i]);
+
+        return ans;
+    }
+}
