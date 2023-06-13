d34c2b71c1c8b47bdb7d5e56297a7ce4647d1c1b
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-13 16:06:16 +0700

    Add files via upload

---
+++ leetcode96.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        for (let j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+    }
+    return dp[n];
+};
