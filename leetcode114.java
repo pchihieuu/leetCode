c803d22960266473839ea27abff87591a2f3c641
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-13 16:19:26 +0700

    Add files via upload

---
+++ leetcode114.java
@@ -0,0 +1,22 @@
+public class leetcode114 {
+    public void flatten(TreeNode root) {
+        if (root == null)
+        return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        TreeNode left = root.left;   // Flattened left
+        TreeNode right = root.right; // Flattened right
+
+        root.left = null;
+        root.right = left;
+
+    // Connect the original right subtree
+    // To the end of new right subtree
+    TreeNode rightmost = root;
+    while (rightmost.right != null)
+      rightmost = rightmost.right;
+    rightmost.right = right;
+    }
+}
