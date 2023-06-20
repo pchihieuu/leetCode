eed0e862781028dffee933d5b06b6c32677ac11c
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-20 21:29:01 +0700

    Add files via upload

---
+++ leetcode377.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+    let dp = new Array(target + 1).fill(0);
+    dp[0] = 1;
+    for (let i = 1; i <= target; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i >= nums[j]) {
+                dp[i] += dp[i - nums[j]];
+            }
+        }
+    }
+    return dp[target];
+};
