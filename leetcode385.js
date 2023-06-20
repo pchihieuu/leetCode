27a55b97ddabc2a673b3ed7148c9347b87521670
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-06-20 21:33:14 +0700

    Add files via upload

---
+++ leetcode385.js
@@ -0,0 +1,59 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     @return {void}
+ *     this.setInteger = function(value) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     @return {void}
+ *     this.add = function(elem) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @param {string} s
+ * @return {NestedInteger}
+ */
+var deserialize = function(s) {
+    let a = JSON.parse(s);
+    return dfs(a);
+};
+
+function dfs(a){
+    if(typeof a === 'number'){
+        let ni = new NestedInteger();
+        ni.setInteger(a);
+        return ni;
+    }
+    let ni = new NestedInteger();
+    for(let i = 0; i < a.length; i++){
+        ni.add(dfs(a[i]));
+    }
+    return ni;
+};
