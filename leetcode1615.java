20e212a9fd8a7ca056493e3ef3971b5195cd1e5f
Phan Chí Hiếu <98179861+pchihieuu@users.noreply.github.com>
2023-08-18 23:29:52 +0700

    Create leetcode1615.java

---
+++ leetcode1615.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        boolean [][] connected = new boolean[n][n];
+        int [] count =  new int[n];
+        for(int[]road : roads) {
+            count[road[0]]++;
+            count[road[1]]++;
+            connected[road[0]][road[1]] = true;
+            connected[road[1]][road[0]] = true;
+
+
+        }
+        int res = 0;
+        for(int i = 0; i < n;i++) {
+            for(int j = i + 1; j < n; j++) {
+                res = Math.max(res, count[i] + count[j]- (connected[i][j]? 1: 0));
+            }
+        }
+        return res;
+    }
+}
